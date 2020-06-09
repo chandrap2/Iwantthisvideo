c955773db94cd5f575c53169d7f9450e49518569
chandrap <chandrap31415@gmail.com>
2020-06-09 01:29:27 -0700

    Asynchronously pushing results to webpage

--- code/server.js
+++ code/server.js
@@ -22,18 +22,17 @@ app.get("/", (request, response) => {
 	response.sendFile("index.html", {root: __dirname + "/../views"});
 
 	T.get("friends/list", {skip_status: true, include_user_entities: false, count: 200}).then((results) => {
-		users = results;
+		users = results.users;
 		valid_users = true;
-		// console.log("done acquiring users", users.users.length);
 	});
-
 });
 
 app.get("/check_users", (request, response) => {
-	result = { users_found: null, num_users: null }
+	result = { users_found: null, num_users: null };
 	if (valid_users) {
 		result.users_found = true;
-		result.num_users = users.users.length;
+		result.num_users = users.length;
+		console.log(result);
 	}
 
 	response.json(result);
@@ -41,19 +40,13 @@ app.get("/check_users", (request, response) => {
 
 // Results page
 app.get("/getvids", (request, response) => {
-	new Promise((res, rej) => {
-		let promiseObj = {resolve: res, reject: rej}
-		update(promiseObj);
-		// update(promiseObj, 5);
-	}).then((results) => {
-		console.log("Done")
-		// response.sendFile("test.html", {root: __dirname + "/../views"});
-		response.json(results);
-		// response.send("Lol");
-	}).catch((err) => {
-		// response.send(err);
-	});
-	// response.sendFile("test.html", {root: __dirname + "/../views"});
+	let i = request.query.user_index;
+	let user = users[i];
+	// console.log(user);
+
+	T.get("statuses/user_timeline",
+	{screen_name: user.screen_name, exclude_replies: true, count: 20})
+	.then(results => response.json(getVids(results)) );
 });
 
 // Twitter authorization
@@ -70,77 +63,40 @@ function update(promiseObj, num_accs = -1) {
 	}).catch(console.error);
 }
 
-// Retrieve Tweets from each user => getVids()
-function saveUsers(data, num_accs, promiseObj) {
-	data = data.users;
-	num_accs = (num_accs == -1) ? data.length : num_accs;
+// Output video URL's from a user's most recent Tweets
+function getVids(results) {
+	let output = { };
 
-	let num = 0, finalObj = {results: []};
+	if (results.length > 0) { // if tweets were returned
+		output.name = results[0].user.name;
+		output.screen_name = results[0].user.screen_name;
+		output.vids = []
+		// output += `${name} ( @${screen_name} ):\n`;
 
-	let processed_accs = 0;
-	// Check if all users have been processed
-	function finishProc(res2) {
-		processed_accs++;
-		new Promise((res1, rej1) => {	if (processed_accs == num_accs) res1();	})
-		.then((results) => { res2(finalObj); });
+		let videos_found = false;
+		for (i in results) { // look at each tweet
+			let entities = results[i].extended_entities
+			if (entities != undefined &&
+			entities.media[0].type == "video") { // if tweet contains video
+				videos_found = true
+
+				let variants = entities.media[0].video_info.variants; // parse through video metadata
+				let max_bitrate = -1
+				let vid_obj = variants[0];
+				for (j in variants) { // output highest quality video url
+					if (variants[j].content_type == "video/mp4" &&
+					variants[j].bitrate > max_bitrate) {
+						vid_obj = variants[j];
+						max_bitrate = variants[j].bitrate;
+					}
+				} // for (j in variants)
+				output.vids.push(`${vid_obj.url}`);
+			} // if (entities != undefined && ...
+		} // for (i in data)
 	}
 
-	// Output video URL's from a user's most recent Tweets
-	function getVids(results) {
-		let output = { };
-
-		if (results.length > 0) { // if tweets were returned
-			output.name = results[0].user.name;
-			output.screen_name = results[0].user.screen_name;
-			output.vids = []
-			// output += `${name} ( @${screen_name} ):\n`;
-
-			let videos_found = false;
-			for (i in results) { // look at each tweet
-				let entities = results[i].extended_entities
-				if (entities != undefined &&
-				entities.media[0].type == "video") { // if tweet contains video
-					videos_found = true
-
-					let variants = entities.media[0].video_info.variants; // parse through video metadata
-					let max_bitrate = -1
-					let vid_obj = variants[0];
-					for (j in variants) { // output highest quality video url
-						if (variants[j].content_type == "video/mp4" &&
-						variants[j].bitrate > max_bitrate) {
-							vid_obj = variants[j];
-							max_bitrate = variants[j].bitrate;
-						}
-					} // for (j in variants)
-					output.vids.push(`${vid_obj.url}`);
-				} // if (entities != undefined && ...
-			} // for (i in data)
-		}
-
-		return output;
-	}
-
-	new Promise((res, rej) => {
-		for (j = 0; j < num_accs; j++) {
-			let friend = data[j];
-			let name = friend.name, screen_name = friend.screen_name;
-			// console.log(name)
-
-			T.get("statuses/user_timeline",
-			{screen_name: screen_name, exclude_replies: true, count: 20}).then((results) => {
-				finalObj.results.push(getVids(results));
-				finishProc(res);
-			}).catch((err) => { // invalid/deleted user
-				console.log(`invalid user: ${name} @(${screen_name})`);
-				finishProc(res);
-			});
-		}
-
-	}).then((results) => {
-		promiseObj.resolve(results);
-	});
+	return output;
 }
-
 
 function test(results) {
 	// console.log(data[0].entities.media)
