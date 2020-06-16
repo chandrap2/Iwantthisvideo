0c012f51ab62490267dba2f356218303b610df7f
chandrap <chandrap31415@gmail.com>
2020-06-16 01:05:18 -0700

    further minimizing account section to only fit account name when collapsed

--- client_scripts/client.js
+++ client_scripts/client.js
@@ -27,7 +27,7 @@ let accTimer = setInterval(() => {
 
 			document.getElementById("loading").remove();
 			input.insertAdjacentElement("beforeend", retrieveBtn);
-			
+
 			clearInterval(accTimer);
 			// ACC_LIMIT = num_accs;
 			ACC_LIMIT = 50;
@@ -43,7 +43,7 @@ function mn() {
 		input.removeChild(retrieveBtn);
 		input.appendChild(loading);
 		results_area.innerHTML = ""; // clearing 'results' section
-		
+
 		for (let i = 0; i < ACC_LIMIT; i++) {
 			let req = new XMLHttpRequest(); // AJAX request for each account
 			req.open("GET", `http://localhost:3001/getvids?acc_index=${i}`)
@@ -51,7 +51,7 @@ function mn() {
 				let results = JSON.parse(req.responseText);
 				outputResults(results);
 				// console.log(results.id); // for comparing response order to request order
-				
+
 				if (results.id == ACC_LIMIT - 1) {
 					/*
 					Event queue order mostly corresponds to 'req.send()' order,
@@ -81,7 +81,7 @@ let outputResults = (data) => {
 		accInfo.innerText = `${acc.name} (@${acc.screen_name})`;
 		result_box.appendChild(accInfo);
 		result_box.appendChild(document.createElement("br"));
-		
+
 		let vid_box;
 		for (let i in acc.vids) {
 			vid_box = document.createElement("video");
@@ -89,13 +89,21 @@ let outputResults = (data) => {
 			vid_box.setAttribute("height", 200);
 			vid_box.setAttribute("controls", true);
 			vid_box.setAttribute("src", acc.vids[i].vid);
+			vid_box.style.display = "none";
 
 			result_box.appendChild(vid_box);
 		}
 		result_box.style.height = "38px";
+
 		result_box.addEventListener("click", () => {
-			let h = result_box.style.height;
-			result_box.style.height = (h == "") ? "38px" : "";
+			dropped_down = (result_box.style.height == "");
+
+			for (let elem of result_box.children) {
+				if (elem.tagName == "VIDEO")
+					elem.style.display = dropped_down ? "none" : "";
+			}
+
+			result_box.style.height = dropped_down ? "38px" : "";
 		});
 
 		results_area.appendChild(result_box);
