7503b42e3486189f578876dfb52312f12c8e2b44
chandrap <chandrap31415@gmail.com>
2020-07-18 23:42:49 -0700

    fix: async connecting to s3/twitter

--- code/server-test.js
+++ code/server-test.js
@@ -1,83 +1,87 @@
 const Twit = require("twitter-lite");
 let T;
 
-const fs = require("fs");
-const token_path = "./twit_auth2.txt";
-
 const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
-const port = 3001;
+// const port = 3001;
 
-app.set("views", __dirname);
-app.use(express.static(__dirname + "/../client_scripts"));
-app.use(express.static(__dirname + "/../styles"));
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-west-2" });
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+const fileParams = { Bucket: "twit-stuff", Key: "twit_auth2.txt" };
+
+app.use(cors({ origin: 'https://master.drw0o7cx6sm26.amplifyapp.com', credentials: true }));
+// app.use(cors({ origin: 'https://master.drw0o7cx6sm26.amplifyapp.com', credentials: true }));
 app.use(cookieParser());
 
-// Listen on port 3001
-// app.listen(port, () => {
-//     console.log(`Server listening at http://localhost:${port}`);
-// });
-
 // app.get("/", (req, res1) => {
-//     res1.sendFile("index.html", { root: __dirname + "/../views" });
+//     res1.send("hello");
+//     // res1.status(200).send('hello world!');
 // });
-
-app.get("/", (req, res1) => {
-    res1.send("hello");
-    // res1.status(200).send('hello world!');
-});
 
 app.get("/clear_cookies", (req, res1) => {
     res1.clearCookie("accToken");
     res1.clearCookie("accTokenSec");
-    res1.json({ });
+    res1.json({});
 });
 
 app.get("/verify", (req, res1) => {
+    // console.log(req);
+    // console.log(res1);
+
     let cks = req.cookies;
     // console.log(cks);
 
     if (cks.accToken &&
-    cks.accToken != "undefined" && cks.accTokenSec != "undefined") {
-        let auth_tokens = readAppToken(token_path);
+        cks.accToken != "undefined" && cks.accTokenSec != "undefined") {
+        let auth_tokens = readAppToken();
         auth_tokens.access_token_key = req.cookies.accToken;
         auth_tokens.access_token_secret = req.cookies.accTokenSec;
         console.log(auth_tokens);
 
         T = new Twit(auth_tokens);
         T.get("account/verify_credentials")
-        .then(res2 => res1.json(res2))
-        .catch(err => console.log(err));
+            .then(res2 => res1.json(res2))
+            .catch(err => console.log(err));
     } else {
-        res1.json({ });
+        res1.json({});
     }
 });
 
-app.get("/get_req_token", (req, res1) => {
-    T = new Twit(readAppToken(token_path));
+// app.use(cors({ origin: 'https://master.drw0o7cx6sm26.amplifyapp.com', credentials: true }));
+app.get("/get_req_token", async (req, res1) => {
+    // console.log(req);
+    // console.log(res1);
+    let consumer_auth = await readAppToken();
+    T = new Twit(consumer_auth);
+    // readAppToken()
+    console.log("twitter:", T);
+    console.log("token:", consumer_auth);
 
-    T.getRequestToken("http://localhost:3001/redir")
-    .then(res2 => res1.json(res2))
-    .catch(console.error);
+    // res1.json({ });
+    let req_token = await T.getRequestToken("https://1poxidle5i.execute-api.us-west-2.amazonaws.com/production/redir");
+    console.log("req:", req_token);
+    res1.json(req_token);
 });
 
 app.get("/redir", (req, res1) => {
-    T = new Twit(readAppToken(token_path));
+    T = new Twit(readAppToken());
 
     let access_auth = req.query;
     T.getAccessToken(access_auth)
-    .then(res2 => {
-        access_auth = readAppToken(token_path);
-        access_auth.access_token_key = res2.oauth_token;
-        access_auth.access_token_secret = res2.oauth_token_secret;
-        T = new Twit(access_auth);
+        .then(res2 => {
+            access_auth = readAppToken();
+            access_auth.access_token_key = res2.oauth_token;
+            access_auth.access_token_secret = res2.oauth_token_secret;
+            T = new Twit(access_auth);
 
-        res1.sendFile("test.html", { root: __dirname + "/../views" });
-        res1.cookie("accToken", res2.oauth_token, { sameSite: true });
-        res1.cookie("accTokenSec", res2.oauth_token_secret, { sameSite: true });
-    })
-    .catch(console.error);
+            res1.sendFile("test.html", { root: __dirname + "/../views" });
+            res1.cookie("accToken", res2.oauth_token, { sameSite: true });
+            res1.cookie("accTokenSec", res2.oauth_token_secret, { sameSite: true });
+        })
+        .catch(console.error);
 });
 
 app.get("/get_accs", (req, res1) => {
@@ -85,38 +89,38 @@ app.get("/get_accs", (req, res1) => {
 
     if (cks.accToken &&
         cks.accToken != "undefined" && cks.accTokenSec != "undefined") {
-        let auth_tokens = readAppToken(token_path);
+        let auth_tokens = readAppToken();
         auth_tokens.access_token_key = req.cookies.accToken;
         auth_tokens.access_token_secret = req.cookies.accTokenSec;
         T = new Twit(auth_tokens);
 
         T.get("friends/list", { skip_status: true, include_user_entities: false, count: 200 })
-        .then(res3 => res1.json( { accs: res3.users } ))
-        .catch(console.error)
+            .then(res3 => res1.json({ accs: res3.users }))
+            .catch(console.error)
     }
 });
 
 app.get("/get_vids", (req, res1) => {
     let name = req.query.acc_name;
     console.log(name);
-    
+
     let cks = req.cookies;
 
     if (cks.accToken &&
         cks.accToken != "undefined" && cks.accTokenSec != "undefined") {
-        let auth_tokens = readAppToken(token_path);
+        let auth_tokens = readAppToken();
         auth_tokens.access_token_key = req.cookies.accToken;
         auth_tokens.access_token_secret = req.cookies.accTokenSec;
         T = new Twit(auth_tokens);
 
         T.get("statuses/user_timeline",
             { screen_name: name, exclude_replies: true, trim_user: true, count: 20 })
-        .then(res3 => {
-            let final = getVids(res3); 
-            final.id = parseInt(req.query.id);
-            res1.json(final);
-        })
-        .catch(err => res1.json({ }) );
+            .then(res3 => {
+                let final = getVids(res3);
+                final.id = parseInt(req.query.id);
+                res1.json(final);
+            })
+            .catch(err => res1.json({}));
     }
 });
 
@@ -153,9 +157,12 @@ function getVids(results) {
     return output;
 }
 
-function readAppToken(path) {
-    let auth_tokens = fs.readFileSync(path, "utf8");
-    return JSON.parse(auth_tokens);
+async function readAppToken() {
+    let file = await s3.getObject(fileParams).promise();
+    let result = JSON.parse(file.Body.toString("utf8"));
+    console.log("data", result);
+
+    return result;
 }
 
 module.exports = app;
